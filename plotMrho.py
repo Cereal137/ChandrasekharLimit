c70afd49618c4ca6f7bc4452de07acfac46600dd
Cereal137 <2100011609@stu.pku.edu.cn>
2023-04-01 15:33:10 +0800

    for HW adjustment

--- plotMrho.py
+++ plotMrho.py
@@ -9,13 +9,12 @@ MRtype = np.dtype({
 #load the data
 D = np.loadtxt('M-R.txt',dtype=MRtype)
 #plot the data
-plt.plot(D['R'],D['M'])
+plt.plot(np.log(D['rho'])/np.log(10),D['M'])
 plt.ylabel('M/M_solar')
-plt.xlabel('R(km)')
-plt.xlim(0,17000)
+plt.xlabel('$lg[ρ_c/(1\mathrm{g/cm^3})]$')
+#plt.xlim(0,17000)
 plt.ylim(0,1.5)
-plt.title('the Mass-Radius relation')
-plt.savefig('ChandrasekharLimit_Mass-Radius.jpg',dpi=400)
+plt.title('the Mass-Density relation')
+plt.savefig('ChandrasekharLimit_Mass-Density.jpg',dpi=400)
 plt.show()
-
 # %%
